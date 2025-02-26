9a4277d2fc3f02800fcf3f7868907d60c61d50c9
Nika <slesareva-n-r@ya.ru>
2025-02-26 14:31:59 +0300

    Initial commit

---
+++ hw_mus_script_1.sql
@@ -0,0 +1,119 @@
+-- Задание 1
+
+INSERT INTO artist(name)
+VALUES ('Ария'), ('Би-2'), ('Слот'), ('Король и шут'), ('BUCK-TICK'), ('X Japan'), ('Daughtry'), ('Thousand Foot Krutch'), ('Пикник');
+
+INSERT INTO genre(name)
+VALUES ('alternative rock'), ('j-rock'), ('heavy-metal'), ('nu metal'), ('punk-rock'), ('rap-metal'), ('hard-rock'), ('post-grunge'), ('pop-rock');
+
+INSERT INTO album(name, year)
+VALUES
+('Герой асфальта', 1987),
+('Gessekai', 1998),
+('Мяу кисс ми', 2001),
+('Жаль, нет ружья', 2002),
+('Abracadabra', 2020),
+('Dearly Beloved', 2021),
+('Железные мантры', 2008)
+('Spitit', 2011),
+('F5', 2011),
+('Феникс', 2011);
+
+INSERT INTO song(name, duration, album_id)
+VALUES
+-- Buck-Tick - Gessekai
+('Gessekai', 306, 2),
+('Muchi no Namida', 388, 2),
+('My Baby Japanese', 268, 2),
+-- Buck-Tick - Abracadabra
+('Peace', 167, 5),
+('Que Sera Sera Elegy', 289, 5),
+('Urahara-Juku', 306, 5),
+('Sophia Dream', 207, 5),
+('Tsuki no Sabaku', 241, 5),
+('Villain', 296, 5),
+('Kogoeru Crystal Cube Ver.', 314, 5),
+('Maimu Maimu', 232, 5),
+('Dance Tengoku', 228, 5),
+-- Ария - Герой Асфальта
+('На службе силы зла', 430, 1),
+('Герой асфальта', 311, 1),
+('Мёртвая зона', 403, 1),
+('1100', 295, 1),
+('Улица роз', 356, 1),
+('Баллада о древнерусском воине', 511, 1),
+-- Daughtry - Dearly Beloved
+('Desperation', 289, 6),
+('World On Fire', 215, 6),
+('Heavy Is The Crown', 235, 6),
+('Changes Are Coming', 221, 6),
+('Dearly Beloved', 223, 6),
+('Cry for Help', 213, 6),
+('Asylum', 235, 6),
+('Evil', 212, 6),
+('Break Into My Heart', 230, 6),
+-- Дополнительно
+('Мой рок-н-ролл', 405, 3),
+('Зажигать', 321, 3),
+('Мертвый анархист', 287, 4),
+('Мой характер', 231, 4),
+('Дом мой на двух ногах', 219, 7);
+
+INSERt iNTO collection(name, year)
+VALUES
+('Japanese rock', 2020),
+('Russian rock', 2002),
+('Grunge & alternative rock', 2021),
+('Art rock', 2010);
+
+INSERT INTO artist_genre(artist_id, genre_id)
+VALUES
+(1, 3),
+(2, 1), (2, 8), (2, 9),
+(3, 1), (3, 4), (3, 6),
+(4, 5),
+(5, 5), (5, 2),
+(6, 2), (6, 3),
+(7, 1), (7, 7), (7, 8),
+(8, 1), (8, 4), (8, 6), (8, 7), (8, 8), (8, 9);
+
+INSERT INTO artist_album(album_id, artist_id)
+VALUES
+(1, 1),
+(2, 5),
+(3, 2),
+(4, 4),
+(5, 5),
+(6, 7),
+(7, 9)
+(8, 2),
+(9, 3),
+(10, 1);
+
+INSERT INTO song_collection(collection_id, song_id)
+VALUES
+-- Japanese rock
+(1, 3),
+(1, 4),
+(1, 6),
+(1, 7),
+(1, 25),
+(1, 26),
+-- Russian rock
+(2, 11),
+(2, 14),
+(2, 15),
+(2, 28),
+(2, 29),
+(2, 30),
+(2, 32),
+-- Grunge & alternative rock
+(3, 18),
+(3, 20),
+(3, 22),
+(3, 17),
+(3, 24),
+-- Art rock
+(4, 28),
+(4, 29),
+(4, 32);
---
+++ hw_mus_script_2.sql
@@ -0,0 +1,107 @@
+-- Задание 2
+
+-- 1. Название и продолжительность самого длительного трека
+SELECT name, duration
+FROM song
+ORDER BY duration DESC
+LIMIT 1;
+
+-- 2. Название треков, продолжительность которых не менее 3,5 минут
+SELECT name, duration
+FROM song
+WHERE duration >= 210
+ORDER BY duration DESC;
+
+-- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно
+SELECT name, year
+FROM collection
+WHERE year BETWEEN 2018 AND 2020;
+
+-- 4. Исполнители, чьё имя состоит из одного слова
+SELECT name
+FROM artist
+WHERE name NOT LIKE '% %' AND name NOT LIKE '%-%';
+
+-- 5. Название треков, которые содержат слово «мой» или «my»
+SELECT name
+FROM song
+WHERE LOWER(name) LIKE '%мой%' OR LOWER(name) LIKE '%my%';
+
+
+-- Задание 3
+
+-- 1. Количество исполнителей в каждом жанре
+SELECT genre.name, COUNT(artist)
+FROM genre
+JOIN artist_genre AS ag ON ag.genre_id = genre.genre_id
+JOIN artist ON ag.artist_id = artist.artist_id
+GROUP BY genre.name
+ORDER BY COUNT(artist);
+
+-- 2. Количество треков, вошедших в альбомы 2019–2020 годов
+SELECT album.name, album.year, COUNT(song) AS song_amount
+FROM album
+JOIN song ON song.album_id = album.album_id
+WHERE album.year BETWEEN 2019 AND 2020
+GROUP BY album.name, album.year;
+
+-- 3. Средняя продолжительность треков по каждому альбому
+SELECT album.name, ROUND(AVG(song.duration)) as avg
+FROM song
+JOIN album ON song.album_id = album.album_id
+GROUP BY album.name
+ORDER BY ROUND(AVG(song.duration));
+
+-- 4. Все исполнители, которые не выпустили альбомы в 2020 году
+SELECT DISTINCT artist.name
+FROM artist_album as aa
+JOIN album ON aa.album_id = album.album_id
+JOIN artist ON aa.artist_id = artist.artist_id
+WHERE album.year != 2020;
+
+-- 5. Названия сборников, в которых присутствует конкретный исполнитель
+SELECT DISTINCT c.name
+FROM collection c
+JOIN song_collection sc ON sc.collection_id = c.collection_id
+JOIN song ON song.song_id = sc.song_id
+JOIN album ON song.album_id = album.album_id
+JOIN artist_album aa ON aa.album_id = album.album_id
+JOIN artist ON aa.artist_id = artist.artist_id
+WHERE artist.name = 'Би-2';
+
+
+-- Задание 4
+
+-- 1. Названия альбомов, в которых присутствуют исполнители более чем одного жанра
+SELECT album.name, COUNT(genre) as genre_count
+FROM artist_album aa
+JOIN artist ON aa.artist_id = artist.artist_id
+JOIN album ON aa.album_id = album.album_id
+JOIN artist_genre ag ON ag.artist_id = artist.artist_id
+JOIN genre ON ag.artist_id = genre.genre_id
+GROUP BY album.name, artist.name
+HAVING COUNT(genre) > 1;
+
+-- 2. Наименования треков, которые не входят в сборники
+SELECT song.name
+FROM song
+LEFT JOIN song_collection sc ON sc.song_id = song.song_id
+WHERE sc.collection_id IS NULL;
+
+-- 3. Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько
+SELECT artist.name, song.name, song.duration
+FROM artist_album as aa
+JOIN artist ON aa.artist_id = artist.artist_id
+JOIN album ON aa.album_id = album.album_id
+JOIN song ON album.album_id = song.album_id
+WHERE song.duration = (SELECT MIN(song.duration) FROM song)
+ORDER BY song.duration;
+
+-- 4. Названия альбомов, содержащих наименьшее количество треков
+SELECT album.name, COUNT(song.name) AS song_amount
+FROM album
+JOIN song ON album.album_id = song.album_id
+GROUP BY album.name
+HAVING COUNT(song.name) > 2 -- чтобы учитывать только полные в БД альбомы
+ORDER BY song_amount;
+
